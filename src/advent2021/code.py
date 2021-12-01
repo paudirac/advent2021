017a7e434ae1179bc8c361e31a138a90efabafb4
Pau Cervera i Badia <pau.cervera@biteright.dental>
2021-12-01 23:21:14 +0100

    Day 1 part 2

--- src/advent2021/code.py
+++ src/advent2021/code.py
@@ -2,20 +2,48 @@ def zip_one(lst):
     return zip(lst, lst[1:])
 
 def count_larger(lst):
-    lst = list(lst)
     diffs = [b-a for a,b in zip_one(lst)]
     return sum(1 for a in diffs if a >0)
 
-def parser(f):
+class Chunks:
+
+    def __init__(self, lst, window):
+        self.lst = list(lst)
+        self.len = len(self.lst)
+        self.window = window
+
+    def __iter__(self):
+        self.i = 0
+        return self
+
+    def __next__(self):
+        if self.i >= self.len - self.window + 1:
+            raise StopIteration
+
+        ret = self.lst[self.i:self.i + self.window]
+        self.i += 1
+        return ret
+
+def sliding_sum(sample, window=1):
+    # for chunk in Chunks(sample, window):
+    #     print(f'{chunk=} {sum(chunk)}')
+    return [sum(chunk) for chunk in Chunks(sample, window=window)]
+
+def int_lines(f):
     for line in f.readlines():
         yield int(line)
 
+def as_list(g):
+    return list(g)
+
 DAYS = {
-    '1': count_larger,
+    '1.1': count_larger,
+    '1.2': lambda l: count_larger(sliding_sum(l, window=3)),
 }
 
 PARSER = {
-    '1': parser,
+    '1.1': lambda f: as_list(int_lines(f)),
+    '1.2': lambda f: as_list(int_lines(f)),
 }
 
 if __name__ == '__main__':
@@ -24,6 +52,6 @@ if __name__ == '__main__':
     _, day = sys.argv
     print(f'{day=}')
     f = sys.stdin
-    parsed = PARSER[day](f)
+    parsed = list(PARSER[day](f))
     answer = DAYS[day](parsed)
     print(f'Day {day} answer: {answer}')
--- src/tests/test_day_1.py
+++ src/tests/test_day_1.py
@@ -1,4 +1,7 @@
-from advent2021.code import count_larger
+from advent2021.code import (
+    count_larger,
+    sliding_sum,
+)
 
 def test_count_larger():
     sample = [
@@ -14,3 +17,43 @@ def test_count_larger():
         263,
     ]
     assert count_larger(sample) == 7
+
+def test_sum_three_window():
+    sample = [
+        199, #  A
+        200, #  A B
+        208, #  A B C
+        210, #    B C D
+        200, #  E   C D
+        207, #  E F   D
+        240, #  E F G
+        269, #    F G H
+        260, #      G H
+        263, #        H
+    ]
+    sample_sliding_sum = [
+        607,
+        618,
+        618,
+        617,
+        647,
+        716,
+        769,
+        792,
+    ]
+    assert sliding_sum(sample, window=3) == sample_sliding_sum
+
+def test_count_larger_sliding():
+    sample = [
+        199, #  A
+        200, #  A B
+        208, #  A B C
+        210, #    B C D
+        200, #  E   C D
+        207, #  E F   D
+        240, #  E F G
+        269, #    F G H
+        260, #      G H
+        263, #        H
+    ]
+    assert count_larger(sliding_sum(sample, window=3)) == 5
